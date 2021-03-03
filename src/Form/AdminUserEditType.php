91c619b605ecad3a12668fbe343261d2efd16604
EliottG <eliott.geoffroy@gmail.com>
2021-03-03 20:33:46 +0100

    Update de la property pour le login username to email + ajout d'un champ de modifications pour l'administrateur

--- src/Form/AdminUserEditType.php
+++ src/Form/AdminUserEditType.php
@@ -5,30 +5,30 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AdminUserEditType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('roles', ChoiceType::class, [
-                'label' => 'Roles de l\'utilisateur',
-                'choices' => ['Administrateur' => 'ROLE_ADMIN'],
-                'required' => true,
-                'multiple' => true,
-                'expanded' => true,
-            ])
-        ;
-    }
+	public function buildForm(FormBuilderInterface $builder, array $options)
+	{
+		$builder
+			->add('username', null, [
+				'required' => false
+			])
+			->add('roles', ChoiceType::class, [
+				'label' => 'Roles de l\'utilisateur',
+				'choices' => ['Administrateur' => 'ROLE_ADMIN'],
+				'required' => true,
+				'multiple' => true,
+				'expanded' => true
+			]);
+	}
 
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults([
-            'data_class' => User::class,
-        ]);
-    }
+	public function configureOptions(OptionsResolver $resolver)
+	{
+		$resolver->setDefaults([
+			'data_class' => User::class
+		]);
+	}
 }
